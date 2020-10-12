d2cf95ee8f0cff7c5814ccb9fc9527bb7189926d
dianemarie-public <69361953+dianemarie-public@users.noreply.github.com>
2020-10-12 13:07:27 -0500

    Updates README and api_keys files

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,7 @@
+# VacationPy
+
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "d324b39ac44ded851a94f0a8c91106d7"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBpfp7TpOXph6l3DgabzvvvGBk3g2vKo6Q"
